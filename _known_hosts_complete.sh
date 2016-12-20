43a5c107088482d31d184846e4ed8a94f9df24c5
Binayak Mishra <bm@Nakamas-MacBook-Pro.local>
2016-12-20 15:07:30 +0700

    Adds comments to known hosts complete config

--- _known_hosts_complete.sh
+++ _known_hosts_complete.sh
@@ -1,2 +1,6 @@
+###
+# add to ~/.bash_profile or add to ~/local/bash/, and close/reopen a shell.
+# will autocomplete any hosts found in known_hosts.
+#
 # | grep -v "\["
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq`;)" ssh
